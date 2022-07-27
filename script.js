88e36556a09f76ba0bfd25a12a919b137a383e65
leowilson <leobca2018@gmail.com>
2022-07-27 22:41:13 +0530

    add-some-functions.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var calculationScreen = document.createElement('p');
 calculationScreen.setAttribute('id', 'ques-screen');
+var time = 5;
 
 var boxDiv = document.getElementById('flex');
 // console.log(boxDiv);
@@ -9,13 +10,14 @@ var quesArrayFirstNum = ['6', '4', '3', '9', '1', '2', '5', '7', '8'];
 var quesArraySecNum = ['3', '8', '5', '2', '1', '9', '4', '7', '6'];
 var empty = [];
 // create answer box 
-var ansArray = ['15', '18', '11', '2', '16', '19', '3', '4', '13', '5', '12', '7', '17', '3', '10', '6', '8', '20', '14', '9'];
+var ansArray = ['15', '18', '11', '2', '16', '19', '3', '4', '13', '5', '12', '7', '17', '1', '10', '6', '8', '20', '14', '9'];
 for (index = 0; index < ansArray.length; index++) {
     var boxCreation = document.createElement('button');
     boxCreation.setAttribute('class', 'boxes');
-    boxCreation.setAttribute('value',ansArray[index]);
-    boxCreation.setAttribute('onclick','valueGet(this.value)');
+    boxCreation.setAttribute('value', ansArray[index]);
+    boxCreation.setAttribute('onclick', 'valueGet(this.value)');
     boxCreation.innerHTML = ansArray[index];
+    boxCreation.disabled = 'true'
     boxDiv.appendChild(boxCreation);
     empty.push(boxCreation);
 }
@@ -30,17 +32,23 @@ var strBtn = document.getElementById('start');
 
 
 // start button function
-strBtn.addEventListener('click', function () {
+strBtn.addEventListener('click', function start() {
     random();
-    show.innerHTML =  quesDisplay;
-    // strBtn.disabled = 'true';
+    show.innerHTML = quesDisplay;
+    for (i = 0; i < empty.length; i++) {
+        // boxCreation = document.createElement('button');
+        empty[i].disabled = '';
+        // console.log(empty[i]);
+    }
+    watch();
+    strBtn.disabled = 'true';
 })
 
 // random number check
 var FirstRandom;
 var secondRandom;
 var total;
-var quesDisplay ;
+var quesDisplay;
 var totalSumEmpty = [];
 function random() {
     //   console.log(quesArrayFirstNum.length); 
@@ -50,37 +58,56 @@ function random() {
 
     var RandomNum_2 = Math.random();
     secondRandom = Math.floor(RandomNum_2 * quesArraySecNum.length);
-      // console.log(secondRandom);
-    if(!(totalSumEmpty.includes(quesDisplay))){
+    // console.log(secondRandom);
+    if (!(totalSumEmpty.includes(quesDisplay))) {
         totalSumEmpty.push(quesDisplay);
         quesDisplay = quesArrayFirstNum[FirstRandom] + ' + ' + quesArraySecNum[secondRandom];
         console.log(quesDisplay);
     }
-    
+
     // find total   
     total = ~~(quesArrayFirstNum[FirstRandom]) + ~~(quesArraySecNum[secondRandom]);
     // console.log(total);
 }
 
+// click answer button
 var ansBtn = document.querySelector('.boxes');
 // console.log(ansBtn);
-// ansBtn.addEventListener('click',valueGet);
-
-function valueGet(val){
-var x = (val);
-// console.log(x);
-
-if( total == val){
-  var y = ansArray.indexOf(x);
-//   console.log(y);
-  empty[y].style.background = 'green';
+function valueGet(val) {
+    var x = (val);
+    time = 5;
+    // console.log(x)
+    if (total == val) {
+        var y = ansArray.indexOf(x);
+        //   console.log(y);
+        empty[y].style.background = 'green';
+    }
+    else {
+        y = ansArray.indexOf(x);
+        empty[y].style.background = 'red';
+    }
+    random();
+    show.innerHTML = quesDisplay;
 }
-else{
-   y = ansArray.indexOf(x);
-   empty[y].style.background = 'red';
+
+
+
+// timer function
+var timeDisplay = document.getElementById('timer');
+// console.log(timeDisplay);
+var timerData;
+// setInterval function
+function watch() {
+    timerData = setInterval(timeset, 1000);
 }
+function timeset() {
+    time--;
+    timeDisplay.innerHTML = time;
+    if (time == 0) {
+        clearInterval(timerData);
+    }
 }
 
-   
+
 
 
