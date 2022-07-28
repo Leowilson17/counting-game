34a9e79573b98177ff35f8838f3b401e6474df5f
leowilson <leobca2018@gmail.com>
2022-07-28 14:47:18 +0530

    add-some-features.

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ var boxDiv = document.getElementById('flex');
 // create ques box
 var quesArrayFirstNum = ['6', '4', '3', '9', '1', '2', '5', '7', '8'];
 var quesArraySecNum = ['3', '8', '5', '2', '1', '9', '4', '7', '6'];
+
 var empty = [];
 // create answer box 
 var ansArray = ['15', '18', '11', '2', '16', '19', '3', '4', '13', '5', '12', '7', '17', '1', '10', '6', '8', '20', '14', '9'];
+// console.log(ansArray);
+
 for (index = 0; index < ansArray.length; index++) {
     var boxCreation = document.createElement('button');
     boxCreation.setAttribute('class', 'boxes');
@@ -26,18 +29,20 @@ for (index = 0; index < ansArray.length; index++) {
 var show = document.getElementById('ans');
 // console.log(show);
 
+
 // start the game
 var strBtn = document.getElementById('start');
 // console.log(strBtn);
 
 
 // start button function
-strBtn.addEventListener('click', function start() {
+strBtn.addEventListener('click', function () {
     random();
     show.innerHTML = quesDisplay;
     for (i = 0; i < empty.length; i++) {
         // boxCreation = document.createElement('button');
         empty[i].disabled = '';
+        empty[i].style.background = '';
         // console.log(empty[i]);
     }
     watch();
@@ -51,26 +56,31 @@ var total;
 var quesDisplay;
 var totalSumEmpty = [];
 function random() {
-    //   console.log(quesArrayFirstNum.length); 
-    var RandomNum_1 = Math.random();
-    FirstRandom = Math.floor(RandomNum_1 * quesArrayFirstNum.length);
-    // console.log(FirstRandom);
 
-    var RandomNum_2 = Math.random();
-    secondRandom = Math.floor(RandomNum_2 * quesArraySecNum.length);
-    // console.log(secondRandom);
-    if (!(totalSumEmpty.includes(total))) {
-        quesDisplay = quesArrayFirstNum[FirstRandom] + ' + ' + quesArraySecNum[secondRandom];
-        totalSumEmpty.push(total);
-        // console.log(quesDisplay);
+    while (totalSumEmpty.length < ansArray.length) {
+        var RandomNum_1 = Math.random();
+        FirstRandom = Math.floor(RandomNum_1 * quesArrayFirstNum.length);
+        // console.log(FirstRandom);
+
+        var RandomNum_2 = Math.random();
+        secondRandom = Math.floor(RandomNum_2 * quesArraySecNum.length);
+        // console.log(secondRandom);
+
+        // find total   
+        total = ~~(quesArrayFirstNum[FirstRandom]) + ~~(quesArraySecNum[secondRandom]);
+        console.log(total);
+
+        if (!(totalSumEmpty.includes(total))) {
+            quesDisplay = quesArrayFirstNum[FirstRandom] + ' + ' + quesArraySecNum[secondRandom];
+            totalSumEmpty.push(total);
+            // console.log(quesDisplay);
+            break;
+        }
     }
-    // find total   
-    total = ~~(quesArrayFirstNum[FirstRandom]) + ~~(quesArraySecNum[secondRandom]);
-    // console.log(total);
-    // console.log(totalSumEmpty);
+    // console.log(totalSumEmpty.length);
 }
 
-// click answer button
+// click answer button function
 var ansBtn = document.querySelector('.boxes');
 // console.log(ansBtn);
 function valueGet(val) {
@@ -83,14 +93,14 @@ function valueGet(val) {
         empty[y].style.background = 'green';
     }
     else {
+        restart();
         y = ansArray.indexOf(x);
         empty[y].style.background = 'red';
-
+        alert('You Lose');
     }
     random();
     show.innerHTML = quesDisplay;
 }
-
 
 
 // timer function
@@ -105,23 +115,25 @@ function timeset() {
     time--;
     timeDisplay.innerHTML = time;
     if (time == 0) {
+        alert('you Lose');
         clearInterval(timerData);
     }
 }
 
 // reset btn function
-var reset = document.getElementById('restart');
+var reset = document.getElementById('stop');
 // console.log(reset);
-reset.addEventListener('click',function(){
+reset.addEventListener('click', restart);
+function restart() {
     clearInterval(timerData);
     time = 10;
     timeDisplay.innerHTML = 0;
-    show.innerHTML = 'Calc';
+    show.innerHTML = 'Lets Start';
     strBtn.disabled = '';
-    for ( var loop = 0; loop < empty.length; loop++){
-    empty[loop].style.background = '';
+    for (var loop = 0; loop < empty.length; loop++) {
+        empty[loop].style.background = '';
     }
-})
+};
 
 
 
